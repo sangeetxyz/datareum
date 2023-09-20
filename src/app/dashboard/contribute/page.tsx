3314e39d6311a31f6a2567fd4f4d76330da9c0e4
onemineus <sangeetbanerjee777@gmail.com>
2023-09-21 02:00:51 +0530

    added custom tooltip

--- src/app/dashboard/contribute/page.tsx
+++ src/app/dashboard/contribute/page.tsx
@@ -101,7 +101,27 @@ const Contribute = () => {
       console.log("wrong format");
     }
   };
+  const CustomTooltip = ({
+    active,
+    payload,
+    label,
+  }: {
+    active: boolean | undefined;
+    payload: any;
+    label: string;
+  }) => {
+    if (active && payload && payload.length > 0) {
+      return (
+        <div className="flex flex-col items-center rounded-lg bg-zinc-950 p-2 text-sm outline outline-1 outline-slate-700">
+          <div>{`On row - ${label + 1}`}</div>
+          <div>{`Raw Columns - ${payload[0].value}`}</div>
+          <div>{`Parsed Columns - ${payload[1].value}`}</div>
+        </div>
+      );
+    }
 
+    return null;
+  };
   useEffect(() => {
     waiter();
     if (!user && !isLoading) {
@@ -192,90 +212,7 @@ const Contribute = () => {
                   </div>
                 </div>
                 <div className="my-4 text-xl uppercase">analytics</div>
-                <div className="h-72 w-full">
-                  <ResponsiveContainer>
-                    <AreaChart
-                      width={500}
-                      height={400}
-                      data={me.splice(0, 100)}
-                      margin={{
-                        top: -30,
-                        right: 0,
-                        left: 0,
-                        bottom: 0,
-                      }}
-                    >
-                      <Tooltip />
-                      <defs>
-                        <linearGradient
-                          id="colorUv"
-                          x1="0"
-                          y1="0"
-                          x2="0"
-                          y2="1"
-                        >
-                          <stop
-                            offset="5%"
-                            stopColor="#8884d8"
-                            stopOpacity={0.8}
-                          />
-                          <stop
-                            offset="95%"
-                            stopColor="#8884d8"
-                            stopOpacity={0}
-                          />
-                        </linearGradient>
-                        <linearGradient
-                          id="colorPv"
-                          x1="0"
-                          y1="0"
-                          x2="0"
-                          y2="1"
-                        >
-                          <stop
-                            offset="5%"
-                            stopColor="#82ca9d"
-                            stopOpacity={0.8}
-                          />
-                          <stop
-                            offset="95%"
-                            stopColor="#82ca9d"
-                            stopOpacity={0}
-                          />
-                        </linearGradient>
-                      </defs>
-                      <Area
-                        type="monotone"
-                        dataKey="rawColumns"
-                        stroke="#4E77FF"
-                        fill="url(#colorUv)"
-                      />
-                      <Area
-                        type="monotone"
-                        dataKey="parsedColumns"
-                        stroke="#FFDCCC"
-                        fill="url(#colorPv)"
-                      />
-                      <Legend
-                        iconType="diamond"
-                        payload={[
-                          {
-                            value: "Raw Columns",
-                            type: "diamond",
-                            id: "rawColumns",
-                            color: "#4E77FF",
-                          },
-                          {
-                            value: "Parsed Columns",
-                            type: "diamond",
-                            id: "parsedColumns",
-                            color: "#FFDCCC",
-                          },
-                        ]}
-                      />
-                    </AreaChart>
-                  </ResponsiveContainer>
-                </div>
+                <AnalyticsSection data={me} />
               </div>
             </div>
           </div>
@@ -316,3 +253,93 @@ const Contribute = () => {
 };
 
 export default Contribute;
+
+const AnalyticsSection = ({ data }: { data: object[] }) => {
+  const CustomTooltip = ({
+    active,
+    payload,
+    label,
+  }: {
+    active: boolean | undefined;
+    payload: any;
+    label: string;
+  }) => {
+    if (active && payload && payload.length > 0) {
+      return (
+        <div className="flex flex-col items-center rounded-lg bg-zinc-950 p-2 text-sm outline outline-1 outline-slate-700">
+          <div>{`On row - ${label + 1}`}</div>
+          <div>{`Raw Columns - ${payload[0].value}`}</div>
+          <div>{`Parsed Columns - ${payload[1].value}`}</div>
+        </div>
+      );
+    }
+
+    return null;
+  };
+  return (
+    <div className="h-72 w-full">
+      <ResponsiveContainer>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data.splice(0, 100)}
+          margin={{
+            top: -26,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <Tooltip
+            content={(event) => (
+              <CustomTooltip
+                active={event.active}
+                label={event.label}
+                payload={event.payload}
+              />
+            )}
+          />
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <Area
+            type="monotone"
+            dataKey="rawColumns"
+            stroke="#4E77FF"
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="parsedColumns"
+            stroke="#FFDCCC"
+            fill="url(#colorPv)"
+          />
+          <Legend
+            iconType="diamond"
+            payload={[
+              {
+                value: "Raw Columns",
+                type: "diamond",
+                id: "rawColumns",
+                color: "#4E77FF",
+              },
+              {
+                value: "Parsed Columns",
+                type: "diamond",
+                id: "parsedColumns",
+                color: "#FFDCCC",
+              },
+            ]}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};

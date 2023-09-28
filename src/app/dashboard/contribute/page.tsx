e013fb429d67101cf7992c858e1a60856af38d0d
onemineus <sangeetbanerjee777@gmail.com>
2023-09-28 23:56:15 +0530

    added butt compo

--- src/app/dashboard/contribute/page.tsx
+++ src/app/dashboard/contribute/page.tsx
@@ -18,19 +18,31 @@ import { AnalyticsSection } from "@/components/contribute/analyticsSection";
 import { RetentionSection } from "@/components/contribute/retentionSection";
 import { FeedbackSection } from "@/components/contribute/feedbackSection";
 import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import ThemeButton from "@/components/custom/themeButton";
+import { Switch } from "@/components/ui/switch";
+import {
   objectEncryptor,
   objectIdentificator,
   objectSplitter,
   objectUserDataMixer,
 } from "@/utils/crypt";
-import axios from "axios";
 import {
   getPatientsDataFromBc,
-  getPatientsDataFromDb,
   handlePatientUploadToBc,
   handlePatientUploadToDb,
 } from "@/utils/handlers";
-import ThemeButton from "@/components/custom/themeButton";
+import { toast } from "react-toastify";
 
 const Contribute = () => {
   const { user } = useAuth();
@@ -162,25 +174,66 @@ const Contribute = () => {
                   rawStats={rawStats}
                   parsedStats={parsedStats}
                 />
-                {/* <div
-                  className="my-4 w-full cursor-pointer text-center"
-                  onClick={async () => {
-                    if (parsedData) {
-                      const d = objectEncryptor(parsedData);
-                      const e = objectIdentificator(d);
-                      const f = objectSplitter(e);
-                      const g = objectUserDataMixer(f.forDb, userData.phone);
-                      // handlePatientUploadToDb(g);
-                      // handlePatientUploadToBc(f.forBc);
-                      console.log(await getPatientsDataFromBc());
-                    }
-                  }}
-                >
-                  yupload
-                </div> */}
-                <div className="mt-8 mb-4 flex items-center w-full rounded-xl overflow-x-clip">
-                  {/* <div>asdasd</div> */}
-                  <ThemeButton title="contribute now" className="w-full"/>
+                <AcceptButton parsedData={parsedData} userData={userData} />
+                <div className="mb-4 mt-8 flex w-full items-center overflow-x-clip rounded-xl">
+                  <AlertDialog>
+                    <AlertDialogTrigger className="w-full">
+                      <ThemeButton title="contribute now" className="w-full" />
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className="md:w-[1000px]">
+                      <AlertDialogHeader>
+                        <AlertDialogTitle className="text-3xl text-zinc-50">
+                          One more step
+                        </AlertDialogTitle>
+                        <AlertDialogDescription className="text-zinc-300">
+                          Read the terms and policies
+                        </AlertDialogDescription>
+                        <div className="flex flex-col space-y-4 pt-2 text-zinc-100">
+                          <div>
+                            By proceeding you are affirming the accuracy and
+                            truthfulness of all the data shared. This commitment
+                            to data accuracy is crucial in advancing the
+                            healthcare sector through research and development
+                            on the Ethereum blockchain.
+                          </div>
+                        </div>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel className="bg-slate-900">
+                          CANCEL
+                        </AlertDialogCancel>
+                        <AlertDialogAction
+                          onClick={async () => {
+                            if (parsedData) {
+                              const p1 = objectEncryptor(parsedData);
+                              const p2 = objectIdentificator(p1);
+                              const p3 = objectSplitter(p2);
+                              const p4 = objectUserDataMixer(
+                                p3.forDb,
+                                userData.phone,
+                              );
+                              await handlePatientUploadToDb(p4);
+                              await handlePatientUploadToBc(p3.forBc);
+                              toast.success("Data uploded!", {
+                                position: "top-right",
+                                autoClose: 5000,
+                                hideProgressBar: false,
+                                closeOnClick: true,
+                                pauseOnHover: false,
+                                draggable: true,
+                                progress: undefined,
+                                pauseOnFocusLoss: false,
+                                theme: "dark",
+                              });
+                            }
+                          }}
+                          className="cursor-pointer rounded-lg bg-gradient-to-tr from-violet-500 to-teal-500 px-3 py-2 text-center text-sm uppercase text-zinc-50 hover:opacity-90 xl:mt-0"
+                        >
+                          accept
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
               </div>
             </div>
@@ -222,3 +275,70 @@ const Contribute = () => {
 };
 
 export default Contribute;
+
+const AcceptButton = ({
+  parsedData,
+  userData,
+}: {
+  parsedData: object[] | null;
+  userData: userData;
+}) => {
+  return (
+    <div className="mb-4 mt-8 flex w-full items-center overflow-x-clip rounded-xl">
+      <AlertDialog>
+        <AlertDialogTrigger className="w-full">
+          <ThemeButton title="contribute now" className="w-full" />
+        </AlertDialogTrigger>
+        <AlertDialogContent className="md:w-[1000px]">
+          <AlertDialogHeader>
+            <AlertDialogTitle className="text-3xl text-zinc-50">
+              One more step
+            </AlertDialogTitle>
+            <AlertDialogDescription className="text-zinc-300">
+              Read the terms and policies
+            </AlertDialogDescription>
+            <div className="flex flex-col space-y-4 pt-2 text-zinc-100">
+              <div>
+                By proceeding you are affirming the accuracy and truthfulness of
+                all the data shared. This commitment to data accuracy is crucial
+                in advancing the healthcare sector through research and
+                development on the Ethereum blockchain.
+              </div>
+            </div>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="bg-slate-900">
+              CANCEL
+            </AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async () => {
+                if (parsedData) {
+                  const p1 = objectEncryptor(parsedData);
+                  const p2 = objectIdentificator(p1);
+                  const p3 = objectSplitter(p2);
+                  const p4 = objectUserDataMixer(p3.forDb, userData.phone);
+                  await handlePatientUploadToDb(p4);
+                  await handlePatientUploadToBc(p3.forBc);
+                  toast.success("Data uploded!", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: true,
+                    progress: undefined,
+                    pauseOnFocusLoss: false,
+                    theme: "dark",
+                  });
+                }
+              }}
+              className="cursor-pointer rounded-lg bg-gradient-to-tr from-violet-500 to-teal-500 px-3 py-2 text-center text-sm uppercase text-zinc-50 hover:opacity-90 xl:mt-0"
+            >
+              accept
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};

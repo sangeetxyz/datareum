7252074eb635ca7af999125bb9a6746fc1753754
onemineus <sangeetbanerjee777@gmail.com>
2023-09-29 01:13:45 +0530

    added contri compo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,17 +7,14 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import CatLoader from "@/components/loaders/catLoader";
 import { ApiSection } from "@/components/dash/apiSection";
-import { motion } from "framer-motion";
 import waves from "../../../public/waves.png";
 import ProfileSection from "@/components/dash/profile";
 import DashHeader from "@/components/headers/dashHeader";
 import { unSigner } from "@/firebase/firebase";
-import Toaster from "@/components/containers/Toaster";
 import God from "@/components/dash/godSection";
 import Container from "@/components/containers/container";
-import ThemeButton from "@/components/custom/themeButton";
-import { BsArrowUpRight, BsFillExclamationDiamondFill } from "react-icons/bs";
-import { toast } from "react-toastify";
+import { ContributionSection } from "@/components/dash/contributionSection";
+
 const Dashboard = () => {
   const { user } = useAuth();
   const router = useRouter();
@@ -70,66 +67,7 @@ const Dashboard = () => {
             <ProfileSection userData={userData} />
             <God userData={userData} />
             <ApiSection userData={userData} />
-            <div>
-              <div className="my-4 text-xl uppercase">contribution</div>
-              <div className="rounded-xl bg-zinc-950 bg-opacity-30 outline outline-2 outline-gray-700 backdrop-blur-sm">
-                <div className="flex items-center justify-between p-6">
-                  <div className="text-xl font-bold">
-                    Contrtibute to the Healthcare now!
-                  </div>
-                  <motion.div
-                    whileHover={{
-                      scale: 1.05,
-                    }}
-                    whileTap={{
-                      scale: 0.95,
-                    }}
-                    className="flex cursor-pointer items-center space-x-2 rounded-lg bg-gradient-to-tr from-violet-500 to-teal-500 px-3 py-2 text-center text-sm uppercase xl:mt-0"
-                    onClick={() => {
-                      if (userData.canContribute) {
-                        router.push("/dashboard/contribute");
-                      } else {
-                        toast.error("You dont have permission!", {
-                          position: "top-right",
-                          autoClose: 5000,
-                          hideProgressBar: false,
-                          closeOnClick: true,
-                          pauseOnHover: false,
-                          draggable: true,
-                          progress: undefined,
-                          pauseOnFocusLoss: false,
-                          theme: "dark",
-                        });
-                      }
-                    }}
-                  >
-                    <div>contribute</div>
-                    <BsArrowUpRight />
-                  </motion.div>
-                </div>
-                <div className="px-6 pb-8">
-                  <div className="flex items-start space-x-2">
-                    <div className="pt-[2px]">
-                      <BsFillExclamationDiamondFill color={"red"} size={20} />
-                    </div>
-                    <div className=" select-none text-justify">
-                      <span className="font-bold">Caution - </span>We only
-                      accept .CSV files and we only proceess a certain type of
-                      schema of data and data types. Please learn more on the{" "}
-                      <span
-                        className="cursor-pointer underline"
-                        onClick={() => {
-                          router.push("/docs");
-                        }}
-                      >
-                        documentation
-                      </span>{" "}
-                      .
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ContributionSection userData={userData} />
           </div>
         </div>
       </div>
@@ -138,3 +76,5 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+
---
+++ src/components/dash/contributionSection.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { userData } from "@/types/types";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { motion } from "framer-motion";
+import { BsArrowUpRight, BsFillExclamationDiamondFill } from "react-icons/bs";
+import { toast } from "react-toastify";
+
+export const ContributionSection = ({ userData }: { userData: userData }) => {
+  const router = useRouter();
+  return (
+    <div>
+      <div className="my-4 text-xl uppercase">contribution</div>
+      <div className="rounded-xl bg-zinc-950 bg-opacity-30 outline outline-2 outline-gray-700 backdrop-blur-sm">
+        <div className="flex items-center justify-between p-6">
+          <div className="text-xl font-bold">
+            Contrtibute to the Healthcare now!
+          </div>
+          <motion.div
+            whileHover={{
+              scale: 1.05,
+            }}
+            whileTap={{
+              scale: 0.95,
+            }}
+            className="flex cursor-pointer items-center space-x-2 rounded-lg bg-gradient-to-tr from-violet-500 to-teal-500 px-3 py-2 text-center text-sm uppercase xl:mt-0"
+            onClick={() => {
+              if (userData.canContribute) {
+                router.push("/dashboard/contribute");
+              } else {
+                toast.error("You dont have permission!", {
+                  position: "top-right",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: false,
+                  draggable: true,
+                  progress: undefined,
+                  pauseOnFocusLoss: false,
+                  theme: "dark",
+                });
+              }
+            }}
+          >
+            <div>contribute</div>
+            <BsArrowUpRight />
+          </motion.div>
+        </div>
+        <div className="px-6 pb-8">
+          <div className="flex items-start space-x-2">
+            <div className="pt-[2px]">
+              <BsFillExclamationDiamondFill color={"red"} size={20} />
+            </div>
+            <div className=" select-none text-justify">
+              <span className="font-bold">Caution - </span>We only accept .CSV
+              files and we only proceess a certain type of schema of data and
+              data types. Please learn more on the{" "}
+              <span
+                className="cursor-pointer underline"
+                onClick={() => {
+                  router.push("/docs");
+                }}
+              >
+                documentation
+              </span>{" "}
+              .
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
